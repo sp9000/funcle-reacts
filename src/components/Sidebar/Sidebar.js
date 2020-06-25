0d0a4819b3916b66b8e787f1a50afdaed688f1b0
sp9000 <sp07@protonmail.com>
2020-06-24 23:21:15 -0400

    removed particle2

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,5 @@
 // @flow strict
 import React from 'react';
-import { render } from "react-dom";
-import Proton from 'proton-engine';
-import RAFManager from 'raf-manager';
-import ParticlesBg from 'particles-bg';
 import Author from './Author';
 import Contacts from './Contacts';
 import Copyright from './Copyright';
@@ -16,41 +12,7 @@ type Props = {
 };
 
 const Sidebar = ({ isIndex }: Props) => {
-  const { author, copyright, menu, particle } = useSiteMetadata();
-  
-    let config = {
-      num: [4, 7],
-      rps: 0.1,
-      radius: [5, 40],
-      life: [1.5, 3],
-      v: [2, 3],
-      tha: [-40, 40],
-      alpha: [0.6, 0],
-      scale: [.1, 0.4],
-      position: "all",
-      color: ["random", "#ff0000"],
-      cross: "dead",
-      // emitter: "follow",
-      random: 15
-    };
-
-    if (Math.random() > 0.85) {
-      config = Object.assign(config, {
-        onParticleUpdate: (ctx, particle) => {
-          ctx.beginPath();
-          ctx.rect(
-            particle.p.x,
-            particle.p.y,
-            particle.radius * 2,
-            particle.radius * 2
-          );
-          ctx.fillStyle = particle.color;
-          ctx.fill();
-          ctx.closePath();
-        }
-      });
-    }
-
+  const { author, copyright, menu } = useSiteMetadata();
   return (
     <div className={styles['sidebar']}>
       <div className={styles['sidebar__inner']}>
@@ -59,7 +21,6 @@ const Sidebar = ({ isIndex }: Props) => {
         <Contacts contacts={author.contacts} />
         <Copyright copyright={copyright} />
       </div>
-      <ParticlesBg type="custom" config={config} bg={true} />
     </div>
   );
 };

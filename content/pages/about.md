0f4615b92aa2596aead9df0ef64ed69391fdfb23
sp9000 <sp07@protonmail.com>
2020-06-25 01:07:25 -0400

    blur

--- content/pages/about.md
+++ content/pages/about.md
@@ -2,13 +2,12 @@
 template: page
 title: About Us
 socialImage: /media/funcle-about-us.jpg
-slug: /about
 draft: false
 ---
 We got our name from a t-shirt that Funcle received on Christmas a few years ago. It was pure cringe. Luckily, we've gottten way more out of that awful t-shirt and are still riding that wave of face palm to this very day.
 
-![Here we are hanging out with some of our relatives. I kinda makes you think how we even survived this long.](/media/funcle-about-us.jpg)
+![Here we are hanging out with some of our relatives. I kinda makes you think; How we even survived this long?](/media/funcle-about-us.jpg)
 
-*Here we are hanging out with some of our relatives. I kinda makes you think how we even survived this long.* 🤔
+*Here we are hanging out with some of our relatives. I kinda makes you think; How did we even survived this long?* 🤔
 
 One day we were sitting around and realized that we both love watching youtube fails, so we decided to try our hand at reviewing some of the best the internet has to offer. Pretty soon, epic TikTok compilations, music videos, challenges, YLYL, LOLZ, and cringelords invaded our studio and had FUNcle teetering on the edge of insanity. Join us for reactions by following us on LBRY and Youtube. Thanks for the sub!
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,5 @@
 // @flow strict
 import React from 'react';
-import Proton from 'proton-engine';
-import RAFManager from 'raf-manager';
-import ParticlesBg from 'particles-bg';
 import Author from './Author';
 import Contacts from './Contacts';
 import Copyright from './Copyright';
@@ -15,41 +12,8 @@ type Props = {
 };
 
 const Sidebar = ({ isIndex }: Props) => {
-  const { author, copyright, menu, particle } = useSiteMetadata();
+  const { author, copyright, menu } = useSiteMetadata();
   
-    let config = {
-      num: [4, 7],
-      rps: 0.1,
-      radius: [5, 40],
-      life: [1.5, 3],
-      v: [2, 3],
-      tha: [-40, 40],
-      alpha: [0.6, 0],
-      scale: [.1, 0.4],
-      position: "all",
-      color: ["random", "#ff0000"],
-      cross: "dead",
-      // emitter: "follow",
-      random: 15
-    };
-
-    if (Math.random() > 0.85) {
-      config = Object.assign(config, {
-        onParticleUpdate: (ctx, particle) => {
-          ctx.beginPath();
-          ctx.rect(
-            particle.p.x,
-            particle.p.y,
-            particle.radius * 2,
-            particle.radius * 2
-          );
-          ctx.fillStyle = particle.color;
-          ctx.fill();
-          ctx.closePath();
-        }
-      });
-    }
-
   return (
     <div className={styles['sidebar']}>
       <div className={styles['sidebar__inner']}>
@@ -58,7 +22,6 @@ const Sidebar = ({ isIndex }: Props) => {
         <Contacts contacts={author.contacts} />
         <Copyright copyright={copyright} />
       </div>
-      <ParticlesBg type="custom" config={config} bg={true} />
     </div>
   );
 };
